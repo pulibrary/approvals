964ab51093f00381e03941914e288b1e591eb594
Carolyn Cole <cam156@psu.edu>
2019-04-18 11:41:27 -0400

    Adding a welcome page with lux components

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,3 +16,20 @@
 // const imagePath = (name) => images(name, true)
 
 console.log('Hello World from Webpacker')
+
+import Vue from "vue/dist/vue.esm"
+import system from "lux-design-system"
+import "lux-design-system/dist/system/system.css"
+import "lux-design-system/dist/system/tokens/tokens.scss"
+
+Vue.use(system)
+
+// create the LUX app and mount it to wrappers with class="lux"
+document.addEventListener("DOMContentLoaded", () => {
+  var elements = document.getElementsByClassName("lux")
+  for (var i = 0; i < elements.length; i++) {
+    new Vue({
+      el: elements[i]
+    })
+  }
+})
