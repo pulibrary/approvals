589dbf1b5766152afc59998d0865ec1112f877f3
Anna Headley <anna3LC@gmail.com>
2019-04-23 10:05:47 -0400

    Generate event scaffold

---
+++ spec/views/events/show.html.erb_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe "events/show", type: :view do
+  before(:each) do
+    @event = assign(:event, Event.create!(
+      :name => "Name",
+      :description => "MyText",
+      :url => "Url"
+    ))
+  end
+
+  it "renders attributes in <p>" do
+    render
+    expect(rendered).to match(/Name/)
+    expect(rendered).to match(/MyText/)
+    expect(rendered).to match(/Url/)
+  end
+end
