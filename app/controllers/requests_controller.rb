e888ebd35d20eb2a1225fb21c7970019a2904058
Anna Headley <anna3LC@gmail.com>
2019-06-06 13:29:33 -0400

    Filters in the controller Working status and request type filters

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -2,6 +2,43 @@ class RequestsController < ApplicationController
   # GET /my_requests
   # GET /my_requests.json
   def my_requests
-    @requests = Request.where(creator: current_staff_profile)
+    @requests = Request.where(my_request_filters)
   end
+
+  private
+
+    def my_request_filters
+      { creator: current_staff_profile }.merge(filters_hash)
+    end
+
+    def filters_hash
+      return {} if request_params[:filters].blank?
+
+      filters = request_params[:filters].to_hash
+      map_request_type_filters(filters)
+    end
+
+    def request_params
+      params.permit(filters: [:status, :request_type])
+    end
+
+    # These couple methods will likely be reused; consider making it into a class like
+    # ParamsToFiltersConverter
+    def map_request_type_filters(filters)
+      request_type_filter = filters.delete("request_type")
+      filters.merge!(request_type_value(request_type_filter)) if request_type_filter.present?
+      filters
+    end
+
+    def request_type_value(param_value)
+      if param_value == "absence"
+        { request_type: "AbsenceRequest" }
+      elsif param_value == "travel"
+        { request_type: "TravelRequest" }
+      elsif Request.travel_categories.values.include?(param_value)
+        { travel_category: param_value }
+      else
+        { absence_type: param_value }
+      end
+    end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -65,4 +65,5 @@ RSpec.configure do |config|
 
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include Devise::Test::IntegrationHelpers, type: :request
+  config.include Devise::Test::IntegrationHelpers, type: :feature
 end
