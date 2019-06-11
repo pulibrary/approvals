980435a4ded6af2e71eb4a4e61546f4c2b7f3cbc
Anna Headley <anna3LC@gmail.com>
2019-06-11 16:57:28 -0400

    Use headless chrome for javascript tests
    fixes #200

---
+++ spec/support/capybara_selenium.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+require "capybara/rspec"
+require "selenium-webdriver"
+
+Capybara.server = :puma, { Silent: true }
+Capybara.register_driver(:selenium) do |app|
+  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+    chromeOptions: { args: %w[headless disable-gpu disable-setuid-sandbox window-size=7680,4320] }
+  )
+  Capybara::Selenium::Driver.new(app,
+                                 browser: :chrome,
+                                 desired_capabilities: capabilities)
+end
+
+Capybara.javascript_driver = :selenium
