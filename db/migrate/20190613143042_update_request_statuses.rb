fc29c8208daf9d9e4fc1b6b68ee084a8418856c6
Carolyn Cole <cam156@psu.edu>
2019-06-13 15:04:49 -0400

    Up is missing The up around the first migration is missing.  It did not impact how it works, but it looks wrong

--- db/migrate/20190613143042_update_request_statuses.rb
+++ db/migrate/20190613143042_update_request_statuses.rb
@@ -1,17 +1,19 @@
 class UpdateRequestStatuses < ActiveRecord::Migration[5.2]
-  remove_column :requests, :status, :request_status
-  execute <<-SQL
-    DROP TYPE request_status;
-    CREATE TYPE request_status AS ENUM ('approved', 'canceled', 'changes_requested', 'denied', 'pending', 'pending_cancelation', 'reported' );
-  SQL
-  add_column :requests, :status, :request_status
-end
+  def up
+    remove_column :requests, :status, :request_status
+    execute <<-SQL
+      DROP TYPE request_status;
+      CREATE TYPE request_status AS ENUM ('approved', 'canceled', 'changes_requested', 'denied', 'pending', 'pending_cancelation', 'reported' );
+    SQL
+    add_column :requests, :status, :request_status
+  end
 
-def down
-  remove_column :requests, :status, :request_status
-  execute <<-SQL
-    DROP TYPE request_status;
-    CREATE TYPE request_status AS ENUM ('pending', 'changes_requested', 'approved', 'denied');
-  SQL
-  add_column :requests, :status, :request_status
+  def down
+    remove_column :requests, :status, :request_status
+    execute <<-SQL
+      DROP TYPE request_status;
+      CREATE TYPE request_status AS ENUM ('pending', 'changes_requested', 'approved', 'denied');
+    SQL
+    add_column :requests, :status, :request_status
+  end
 end
