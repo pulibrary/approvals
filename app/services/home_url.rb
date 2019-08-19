f0ffba20ac97c028c52b9f39ef862922cc9d1609
Carolyn Cole <cam156@psu.edu>
2019-08-19 13:33:52 -0400

    Changing home link based on if you are logged in Added a service that could be used to generate a different url based on if someone is logged in or not Connected that service into the view refs #26

---
+++ app/services/home_url.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+class HomeURL
+  class << self
+    def for(current_user:)
+      return Rails.application.routes.url_helpers.root_path if current_user.blank?
+
+      Rails.application.routes.url_helpers.my_requests_path
+    end
+  end
+end
