891ee33e47195ef719ab2d0788217c88f39ef9b2
Carolyn Cole <cam156@psu.edu>
2019-08-27 15:16:23 -0400

    Adding a description field to the cost estimate model

--- db/structure.sql
+++ db/structure.sql
@@ -172,7 +172,8 @@ CREATE TABLE public.estimates (
     recurrence integer,
     created_at timestamp without time zone NOT NULL,
     updated_at timestamp without time zone NOT NULL,
-    cost_type public.estimate_cost_type
+    cost_type public.estimate_cost_type,
+    description character varying
 );
 
 
@@ -769,6 +770,7 @@ INSERT INTO "schema_migrations" (version) VALUES
 ('20190613173252'),
 ('20190613185106'),
 ('20190614132041'),
-('20190718132916');
+('20190718132916'),
+('20190827172900');
 
 
--- spec/models/estimate_spec.rb
+++ spec/models/estimate_spec.rb
@@ -8,5 +8,6 @@ RSpec.describe Estimate, type: :model do
     it { is_expected.to respond_to :cost_type }
     it { is_expected.to respond_to :amount }
     it { is_expected.to respond_to :recurrence }
+    it { is_expected.to respond_to :description }
   end
 end
