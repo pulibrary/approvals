6eb491ca9ac9ae16ae7ce521c67f3780ecfd3cc8
Carolyn Cole <cac9@princeton.edu>
2019-12-04 12:23:39 -0500

    Adding logout button

--- spec/views/welcome/index.html.erb_spec.rb
+++ spec/views/welcome/index.html.erb_spec.rb
@@ -3,8 +3,21 @@ require "rails_helper"
 
 RSpec.describe "welcome/index", type: :view do
   it "renders attributes in <p>" do
+    without_partial_double_verification do
+      allow(view).to receive(:current_user).and_return(nil)
+      render
+    end
     render
     expect(rendered).to include("Leave and Travel Requests")
     expect(rendered).to have_selector("hyperlink[href=\"/users/auth/cas\"]", text: "LOGIN with NetID")
+  end
+
+  it "renders logout if current profile is set" do
+    without_partial_double_verification do
+      allow(view).to receive(:current_user).and_return("abc")
+      render
+    end
+    expect(rendered).to include("Leave and Travel Requests")
+    expect(rendered).to have_selector("hyperlink[href=\"/sign_out\"]", text: "LOGOUT")
   end
 end
