6c0ef6845a9a4400592bb04f39572b0a74095638
Carolyn Cole <cac9@princeton.edu>
2019-12-09 11:17:37 -0500

    Adding coverage for the delegate in the controller

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -11,4 +11,33 @@ RSpec.describe ApplicationController do
       expect(controller.new_session_path(nil)).to eq("/sign_in")
     end
   end
+
+  describe "#current_delegate" do
+    it "returns a good delegate" do
+      controller = described_class.new
+      current_user = FactoryBot.create :staff_profile
+      delegate_staff_profile = FactoryBot.create :staff_profile
+      delegate = FactoryBot.create :delegate, delegator: delegate_staff_profile, delegate: current_user
+      allow(controller).to receive(:session).and_return(approvals_delegate: delegate.id)
+      allow(controller).to receive(:current_user).and_return(current_user.user)
+      expect(controller.current_delegate).to eq(delegate)
+    end
+
+    it "returns nil for a bad delegate" do
+      controller = described_class.new
+      allow(controller).to receive(:session).and_return(approvals_delegate: "abc")
+      expect(controller.current_delegate).to be_nil
+    end
+
+    it "returns nil for a delegate that does not include the current_user" do
+      controller = described_class.new
+      current_user = FactoryBot.create :staff_profile
+      staff_profile = FactoryBot.create :staff_profile
+      delegate_staff_profile = FactoryBot.create :staff_profile
+      delegate = FactoryBot.create :delegate, delegator: delegate_staff_profile, delegate: staff_profile
+      allow(controller).to receive(:session).and_return(approvals_delegate: delegate.id)
+      allow(controller).to receive(:current_user).and_return(current_user)
+      expect(controller.current_delegate).to be_nil
+    end
+  end
 end
