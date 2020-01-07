6dda5c850223c9da1a73937865147a26c4f5329a
Axa Mei Liauw <aliauw@princeton.edu>
2020-01-07 11:17:41 -0500

    Hides new leave request button and leave request type filter options

--- spec/features/my_approval_requests_spec.rb
+++ spec/features/my_approval_requests_spec.rb
@@ -13,7 +13,8 @@ RSpec.feature "My Approval Requests", type: :feature, js: true do
 
   scenario "I can filter my requests" do
     FactoryBot.create(:absence_request, creator: employee, start_date: Date.parse("2019-10-21"), end_date: Date.parse("2019-10-23"))
-    sick_leave = FactoryBot.create(:absence_request, creator: employee, absence_type: "sick", start_date: Date.parse("2019-10-21"), end_date: Date.parse("2019-10-23"))
+    # sick_leave = FactoryBot.create(:absence_request, creator: employee, absence_type: "sick", start_date: Date.parse("2019-10-21"), end_date: Date.parse("2019-10-23"))
+    FactoryBot.create(:absence_request, creator: employee, absence_type: "sick", start_date: Date.parse("2019-10-21"), end_date: Date.parse("2019-10-23"))
     recurring_event = FactoryBot.create(:recurring_event, name: "Awesome Event", description: "The most awesome event!!!")
     event_request = FactoryBot.build(:event_request, recurring_event: recurring_event, start_date: Date.parse("2019-10-21"), end_date: Date.parse("2019-10-23"))
     FactoryBot.create(:travel_request, creator: employee,
@@ -29,17 +30,17 @@ RSpec.feature "My Approval Requests", type: :feature, js: true do
     assert_selector ".my-request .lux-card", count: 1
     assert_selector "a", text: "Doe, Jane (jdoe) - Awesome Event 2019, Location"
 
-    select_drop_down(menu: "#request-type-menu", item: "Absence")
-    assert_selector ".my-request .lux-card", count: 2
-    assert_selector "a", text: "Doe, Jane (jdoe) - Sick"
+    # select_drop_down(menu: "#request-type-menu", item: "Absence")
+    # assert_selector ".my-request .lux-card", count: 2
+    # assert_selector "a", text: "Doe, Jane (jdoe) - Sick"
 
-    click_link("Request type: Absence")
-    assert_selector ".my-request .lux-card", count: Request.count
+    # click_link("Request type: Absence")
+    # assert_selector ".my-request .lux-card", count: Request.count
 
-    click_link "Doe, Jane (jdoe) - Sick"
-    assert_selector "h1", text: "Review Leave Request"
-    click_on "Approve"
-    expect(sick_leave.reload).to be_approved
+    # click_link "Doe, Jane (jdoe) - Sick"
+    # assert_selector "h1", text: "Review Leave Request"
+    # click_on "Approve"
+    # expect(sick_leave.reload).to be_approved
   end
 
   scenario "I can search my requests" do
@@ -52,10 +53,10 @@ RSpec.feature "My Approval Requests", type: :feature, js: true do
     visit "/my_approval_requests"
 
     # filter with out query
-    select_drop_down(menu: "#request-type-menu", item: "Absence")
-    assert_selector ".my-request .lux-card", count: 2
-    ids = page.all(:css, ".my-request .lux-card").map { |element| element["id"].to_i }
-    expect(ids).to contain_exactly absence_request.id, absence_request2.id
+    # select_drop_down(menu: "#request-type-menu", item: "Absence")
+    # assert_selector ".my-request .lux-card", count: 2
+    # ids = page.all(:css, ".my-request .lux-card").map { |element| element["id"].to_i }
+    # expect(ids).to contain_exactly absence_request.id, absence_request2.id
 
     # search query clears the filter
     fill_in "query", with: "balloons"
@@ -67,14 +68,14 @@ RSpec.feature "My Approval Requests", type: :feature, js: true do
     expect(ids).to contain_exactly absence_request.id, travel_request.id
 
     # filter with query
-    select_drop_down(menu: "#request-type-menu", item: "Absence")
+    select_drop_down(menu: "#request-type-menu", item: "Travel")
     expect(find("#query").value).to eq "balloons"
     assert_selector ".my-request .lux-card", count: 1
     ids = page.all(:css, ".my-request .lux-card").map { |element| element["id"].to_i }
-    expect(ids).to contain_exactly absence_request.id
+    expect(ids).to contain_exactly travel_request.id
 
     # clear the filter
-    click_link("Request type: Absence")
+    click_link("Request type: Travel")
 
     # clearing cilter retains search results
     expect(find("#query").value).to eq "balloons"
