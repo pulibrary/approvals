4306131a095c6a09f7bc3d76cebd237663e690b2
Carolyn Cole <cac9@princeton.edu>
2020-01-09 08:38:43 -0500

    Adding seeding of recurring events

---
+++ app/services/recurring_event_loader.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+class RecurringEventLoader
+  class << self
+    def load(config_file: Rails.root.join("config", "recurring_events.yml"))
+      yaml = YAML.safe_load(File.read(config_file))
+      yaml.each do |name|
+        event = RecurringEvent.where(name: name).first
+        event ||= RecurringEvent.new(name: name)
+        event.save!
+      end
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -20,7 +20,5 @@ puts errors.inspect
 # process the seeds a second time to put in the AAs since they should now exist
 LocationLoader.load
 
-RecurringEvent.create(name: "Code4Lib", url: "https://code4lib.org/", 
-  description: "A volunteer-driven collective of hackers, designers, architects, curators, catalogers, artists and instigators from around the world, who largely work for and with libraries, archives and museums on technology “stuff.”")
-RecurringEvent.create(name: "LDCX", url: "https://library.stanford.edu/projects/ldcx", 
-    description: "LDCX is an annual unconference that brings together leading technologists in the libraries, archives and museums (LAM) spaces, to work collaboratively on common needs.")
+# process recurring events
+RecurringEventLoader.load
