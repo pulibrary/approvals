a5f1c708dde286f6216b26a40aadc97d61902652
Carolyn Cole <cac9@princeton.edu>
2020-02-26 13:40:14 -0500

    Allowing the tree of supervisors under the current supervisor for absent staff

--- app/services/absent_staff.rb
+++ app/services/absent_staff.rb
@@ -3,15 +3,14 @@ class AbsentStaff
   class << self
     def list(start_date:, end_date:, supervisor:)
       request_creator_join = AbsenceRequest.joins(:creator)
-      supervisors = supervisor.list_supervised(list: [supervisor]).select(&:supervisor?)
+      supervisors = supervisor.list_supervised(list: []).select(&:supervisor?) | [supervisor]
       absence_requests =
         request_creator_join.where(start_date: start_date..end_date, 'staff_profiles.supervisor_id': supervisors.map(&:id)).or(
           request_creator_join.where(end_date: start_date..end_date, 'staff_profiles.supervisor_id': supervisors.map(&:id))
-        ) #.or(
-        #   request_creator_join.where("start_date <= ? and end_date >= ? and staff_profiles.supervisor_id = ?",
-        #                              start_date, end_date, supervisor)
-        # )
-
+        ).or(
+          request_creator_join.where("start_date <= ? and end_date >= ? and staff_profiles.supervisor_id in (?)",
+                                     start_date, end_date, supervisors.map(&:id))
+        )
       absence_requests.map(&:creator)
     end
   end
--- spec/services/absent_staff_spec.rb
+++ spec/services/absent_staff_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe AbsentStaff, type: :model do
   let(:jill) { FactoryBot.create :staff_profile, given_name: "Jill", supervisor: supervisor }
   let(:mary) { FactoryBot.create :staff_profile, given_name: "Mary", supervisor: jill }
   let(:peter) { FactoryBot.create :staff_profile, given_name: "Peter", supervisor: supervisor }
+  let(:piper) { FactoryBot.create :staff_profile, given_name: "Piper", supervisor: peter }
   let(:mary_sick_2020) do
     start_date = Time.zone.now + 1.year
     end_date = start_date + 2.days
@@ -32,22 +33,28 @@ RSpec.describe AbsentStaff, type: :model do
     end_date = start_date + 4.days
     FactoryBot.create :absence_request, creator: jill, start_date: start_date, end_date: end_date
   end
+  let(:piper_vacation_2019) do
+    start_date = Time.zone.now - 3.days
+    end_date = start_date + 4.days
+    FactoryBot.create :absence_request, creator: piper, start_date: start_date, end_date: end_date
+  end
 
   before do
     mary_sick_2019
     jack_vacation_2019
     jill_vacation_2019
+    piper_vacation_2019
     mary_sick_2020
     peter_sick_2020
   end
 
   describe "#list" do
     it "returns absent staff" do
-      expect(AbsentStaff.list(start_date: Time.zone.now, end_date: Time.zone.now + 2.days, supervisor: supervisor)).to contain_exactly(mary, jack, jill)
+      expect(AbsentStaff.list(start_date: Time.zone.now, end_date: Time.zone.now + 2.days, supervisor: supervisor)).to contain_exactly(mary, jack, jill, piper)
     end
 
     it "returns absent staff when window is inside the staff absence" do
-      expect(AbsentStaff.list(start_date: Time.zone.now - 2.days, end_date: Time.zone.now - 1.day, supervisor: supervisor)).to contain_exactly(jill)
+      expect(AbsentStaff.list(start_date: Time.zone.now - 2.days, end_date: Time.zone.now - 1.day, supervisor: supervisor)).to contain_exactly(jill, piper)
     end
 
     it "returns absent staff when window is around end date of the absence" do
@@ -55,7 +62,7 @@ RSpec.describe AbsentStaff, type: :model do
     end
 
     it "returns no one when the supervisor is other" do
-      expect(AbsentStaff.list(start_date: Time.zone.now + 2.days, end_date: Time.zone.now + 5.days, supervisor: peter)).to be_empty
+      expect(AbsentStaff.list(start_date: Time.zone.now + 2.days, end_date: Time.zone.now + 5.days, supervisor: piper)).to be_empty
     end
   end
 end
