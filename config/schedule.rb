290b0e7d9c7475c4e952d299fe89a41439363896
Kevin Reiss <kevin.reiss@gmail.com>
2023-04-06 13:21:53 -0500

    Adjust scheduled staff list to occur after the new staff list is posted at 5 a.m. eastern.

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 env :PATH, ENV["PATH"]
 
-every :day, at: "12:20am", roles: [:app] do
+# Run at 6:05 am EST or 7:05 EDT (after the 5am staff report is generated)
+every :day, at: "11:05 am", roles: [:app] do
   rake "approvals:process_reports", output: "log/cron.log"
 end
