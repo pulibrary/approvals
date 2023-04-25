cdbdaf50c32fd3556e59883b7a1a399800ccd9d6
Max Kadel <mkadel@princeton.edu>
2023-04-25 14:11:47 -0400

    Get tests green locally

--- spec/services/ldap_spec.rb
+++ spec/services/ldap_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Ldap, type: :model do
     # this test can not be run on circle (tagged :no_ci to not run on circle ci)
     it "returns json ldap data about the user when connected to the actual ldap (you must be on campus or VPN for this to work)", :no_ci do
       expect(described_class.find_by_netid("cac9")).to eq(address: "Firestone Library$Library Information Technology",
-                                                          department: "Library - Deputy University Library",
+                                                          department: "Library - Office of the Deputy University Librarian",
                                                           netid: "cac9")
     end
 
@@ -19,11 +19,11 @@ RSpec.describe Ldap, type: :model do
 
       it "returns json about the user" do
         allow(entry).to receive(:[]).with(:uid).and_return(["cac9"])
-        allow(entry).to receive(:[]).with(:ou).and_return(["Library - Deputy University Library"])
+        allow(entry).to receive(:[]).with(:ou).and_return(["Library - Office of the Deputy University Librarian"])
         allow(entry).to receive(:[]).with(:puinterofficeaddress).and_return(["Firestone Library$Library Information Technology"])
 
         expect(described_class.find_by_netid("cac9", ldap_connection: connection)).to eq(address: "Firestone Library$Library Information Technology",
-                                                                                         department: "Library - Deputy University Library",
+                                                                                         department: "Library - Office of the Deputy University Librarian",
                                                                                          netid: "cac9")
       end
     end
