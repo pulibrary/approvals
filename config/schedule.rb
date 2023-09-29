dafecb7884e855a1a2ab3458b7030d35870e845e
Kevin Reiss <kevin.reiss@gmail.com>
2023-09-29 08:25:31 -0500

    OIT advises they needed to change the report run time to 8 a.m. Updating schedule. (#939)

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 env :PATH, ENV["PATH"]
 
-# Run at 6:05 am EST or 7:05 EDT (after the 5am staff report is generated)
-every :day, at: "11:05 am", roles: [:app] do
+# Run at 9:05 am EST or 10:05 EDT (after the 8am staff report is generated)
+every :day, at: "02:05 pm", roles: [:app] do
   rake "approvals:process_reports", output: "log/cron.log"
 end
