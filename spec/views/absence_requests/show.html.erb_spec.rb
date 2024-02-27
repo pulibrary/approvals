5ab862efa913d90978d9fbbe56c94fbe9a5a93f0
Max Kadel <mkadel@princeton.edu>
2024-02-27 16:37:08 -0500

    Fix views/absence_requests specs (#1031)
    Closes #1012

--- spec/views/absence_requests/show.html.erb_spec.rb
+++ spec/views/absence_requests/show.html.erb_spec.rb
@@ -17,9 +17,9 @@ RSpec.describe "absence_requests/show", type: :view do
     expect(rendered).to match(/Vacation \(#{Time.zone.now.strftime('%m/%d/%Y')} to #{Time.zone.tomorrow.strftime('%m/%d/%Y')}\)/)
     expect(rendered).to include("Sally Smith on")
     expect(rendered).to include(absence_request.notes.first.content)
-    expect(rendered).to have_selector("hyperlink[href=\"#{edit_absence_request_path(absence_request.id)}\"]", text: "Edit")
+    expect(rendered).to have_selector("lux-hyperlink[href=\"#{edit_absence_request_path(absence_request.id)}\"]", text: "Edit")
     expect(rendered).to have_selector("form[action=\"#{decide_absence_request_path(absence_request.id)}\"]")
-    expect(rendered).to have_selector("input-button", text: "Cancel")
+    expect(rendered).to have_selector("lux-input-button", text: "Cancel")
   end
 
   context "not the creator" do
@@ -28,7 +28,7 @@ RSpec.describe "absence_requests/show", type: :view do
         allow(view).to receive(:current_staff_profile).and_return(nil)
         render
       end
-      expect(rendered).not_to have_selector("hyperlink[href=\"#{edit_absence_request_path(absence_request.id)}\"]", text: "Edit")
+      expect(rendered).not_to have_selector("lux-hyperlink[href=\"#{edit_absence_request_path(absence_request.id)}\"]", text: "Edit")
       expect(rendered).not_to have_selector("form[action=\"#{decide_absence_request_path(absence_request.id)}\"]")
     end
   end
