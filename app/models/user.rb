b4fb5cb0df7239675ce4df634b0c595e3be80ef6
Jane Sandberg <sandbergja@users.noreply.github.com>
2024-08-16 15:28:58 -0700

    [#1173] Handle case where CAS returned a netid in all caps (#1174)

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   has_one :staff_profile
 
   def self.from_cas(access_token)
-    User.find_by(provider: access_token.provider, uid: access_token.uid)
+    User.find_by(provider: access_token.provider, uid: access_token.uid&.downcase)
   end
 
   def to_s
