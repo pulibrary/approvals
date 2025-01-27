c82deab6802ab4f96da99a1c9fe5e14c47ae33bd
Christina Chortaria <actspatial@gmail.com>
2025-01-27 15:44:21 -0500

    Make jstroop Jane's supervisor
    Co-authored-by: Jane Sandberg <sandbergja@users.noreply.github.com>

--- db/structure.sql
+++ db/structure.sql
@@ -10,13 +10,6 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
--- Name: public; Type: SCHEMA; Schema: -; Owner: -
---
-
--- *not* creating schema, since initdb creates it
-
-
---
 -- Name: estimate_cost_type; Type: TYPE; Schema: public; Owner: -
 --
 
