4ca486d592682df18dbcff73106c09ef73355914
Jane Sandberg <js7389@princeton.edu>
2026-09-22 15:10:51 -0700

    Add more information from ldap for troubleshooting purposes

--- app/services/ldap.rb
+++ app/services/ldap.rb
@@ -4,12 +4,18 @@ class Ldap
   class << self
     def find_by_netid(net_id, ldap_connection: default_connection)
       filter = Net::LDAP::Filter.eq("uid", net_id)
-      result = ldap_connection.search(filter:).first
-      {
-        netid: result[:uid].first,
-        department: result[:ou].first,
-        address: result[:puinterofficeaddress].first
-      }
+      results =  ldap_connection.search(filter:)
+      if results.empty?
+        raise "Could not find ldap info for #{net_id}"
+      else
+        puts "FOUND LDAP FOR #{net_id}"
+        result = results.first
+        {
+          netid: result[:uid].first,
+          department: result[:ou].first,
+          address: result[:puinterofficeaddress].first
+        }
+      end
     end
 
     private
